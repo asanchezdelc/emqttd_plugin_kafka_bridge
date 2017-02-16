ccb5f9582cfacd9d8b0961dc659186a0b2e1ff49
Zarathos94 <irfansaric94@gmail.com>
2017-02-16 15:55:55 +0100

    Change

--- src/emqttd_plugin_kafka_bridge.erl
+++ src/emqttd_plugin_kafka_bridge.erl
@@ -1,24 +1,4 @@
 %%%-----------------------------------------------------------------------------
-%%% Copyright (c) 2016 Huang Rui<vowstar@gmail.com>, All Rights Reserved.
-%%%
-%%% Permission is hereby granted, free of charge, to any person obtaining a copy
-%%% of this software and associated documentation files (the "Software"), to deal
-%%% in the Software without restriction, including without limitation the rights
-%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-%%% copies of the Software, and to permit persons to whom the Software is
-%%% furnished to do so, subject to the following conditions:
-%%%
-%%% The above copyright notice and this permission notice shall be included in all
-%%% copies or substantial portions of the Software.
-%%%
-%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-%%% SOFTWARE.
-%%%-----------------------------------------------------------------------------
 %%% @doc
 %%% emqttd_plugin_kafka_bridge.
 %%%
@@ -64,7 +44,7 @@ on_client_connected(ConnAck, Client = #mqtt_client{client_id  = ClientId}, _Env)
     %io:format("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
 
     Json = mochijson2:encode([
-        {type, <<"connected">>},
+        {type, <<"client_connected">>},
         {client_id, ClientId},
         {cluster_node, node()},
         {ts, emqttd_time:now_to_secs()}
@@ -84,7 +64,7 @@ on_client_disconnected(Reason, _Client = #mqtt_client{client_id = ClientId}, _En
     %io:format("client ~s disconnected, reason: ~w~n", [ClientId, Reason]),
 
     Json = mochijson2:encode([
-        {type, <<"disconnected">>},
+        {type, <<"client_disconnected">>},
         {client_id, ClientId},
         {reason, Reason},
         {cluster_node, node()},
@@ -110,7 +90,7 @@ on_client_subscribe(ClientId, Username, TopicTable, _Env) ->
             Key = proplists:get_keys(TopicTable),
             %% build json to send using ClientId
             Json = mochijson2:encode([
-                {type, <<"subscribed">>},
+                {type, <<"client_subscribed">>},
                 {client_id, ClientId},
                 {username, Username},
                 {topic, lists:last(Key)},
@@ -131,14 +111,14 @@ on_client_subscribe(ClientId, Username, TopicTable, _Env) ->
 %%-----------client unsubscribed start----------------------------------------%%
 
 on_client_unsubscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("client ~s(~s) unsubscribe ~p~n", [ClientId, Username, TopicTable]),
+    %io:format("client ~s(~s) unsubscribe ~p~n", [ClientId, Username, TopicTable]),
     case TopicTable of
         [_|_] -> 
             %% If TopicTable list is not empty
             Key = proplists:get_keys(TopicTable),
             %% build json to send using ClientId
             Json = mochijson2:encode([
-                {type, <<"unsubscribed">>},
+                {type, <<"client_unsubscribed">>},
                 {client_id, ClientId},
                 {username, Username},
                 {topic, lists:last(Key)},
@@ -161,7 +141,7 @@ on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env)
     {ok, Message};
 
 on_message_publish(Message, _Env) ->
-    io:format("publish ~s~n", [emqttd_message:format(Message)]),   
+    %io:format("publish ~s~n", [emqttd_message:format(Message)]),   
 
     {ClientId, Username} = Message#mqtt_message.from,
     %Sender =  Message#mqtt_message.sender,
@@ -171,7 +151,7 @@ on_message_publish(Message, _Env) ->
     Timestamp = Message#mqtt_message.timestamp,
 
     Json = mochijson2:encode([
-        {type, <<"published">>},
+        {type, <<"message_published">>},
         {client_id, ClientId},
         {username, Username},
         {topic, Topic},
@@ -186,7 +166,7 @@ on_message_publish(Message, _Env) ->
     {ok, Message}.
 
 on_session_created(ClientId, Username, _Env) ->
-    io:format("session(~s/~s) created.", [ClientId, Username]),
+    %io:format("session(~s/~s) created.", [ClientId, Username]),
     Json = mochijson2:encode([
         {type, <<"session_created">>},
         {client_id, ClientId},
@@ -197,9 +177,9 @@ on_session_created(ClientId, Username, _Env) ->
     ekaf:produce_async_batched(<<"broker_message">>, list_to_binary(Json)).
 
 on_session_subscribed(ClientId, Username, {Topic, Opts}, _Env) ->
-    io:format("session(~s/~s) subscribed: ~p~n", [Username, ClientId, {Topic, Opts}]),
+    %io:format("session(~s/~s) subscribed: ~p~n", [Username, ClientId, {Topic, Opts}]),
     Json = mochijson2:encode([
-        {type, <<"session_created">>},
+        {type, <<"session_subscribed">>},
         {client_id, ClientId},
         {username, Username},
         {topic, Topic},
@@ -210,7 +190,7 @@ on_session_subscribed(ClientId, Username, {Topic, Opts}, _Env) ->
     {ok, {Topic, Opts}}.
 
 on_session_unsubscribed(ClientId, Username, {Topic, Opts}, _Env) ->
-    io:format("session(~s/~s) unsubscribed: ~p~n", [Username, ClientId, {Topic, Opts}]),
+    %io:format("session(~s/~s) unsubscribed: ~p~n", [Username, ClientId, {Topic, Opts}]),
     Json = mochijson2:encode([
         {type, <<"session_unsubscribed">>},
         {client_id, ClientId},
@@ -223,7 +203,7 @@ on_session_unsubscribed(ClientId, Username, {Topic, Opts}, _Env) ->
     ok.
 
 on_session_terminated(ClientId, Username, Reason, _Env) ->
-    io:format("session(~s/~s) terminated: ~p.", [ClientId, Username, Reason]),
+    %io:format("session(~s/~s) terminated: ~p.", [ClientId, Username, Reason]),
     Json = mochijson2:encode([
         {type, <<"session_terminated">>},
         {client_id, ClientId},
@@ -237,7 +217,7 @@ on_session_terminated(ClientId, Username, Reason, _Env) ->
 
 %%-----------message delivered start--------------------------------------%%
 on_message_delivered(ClientId, Username, Message, _Env) ->
-    io:format("delivered to client ~s: ~s~n", [ClientId, emqttd_message:format(Message)]),
+    %io:format("delivered to client ~s: ~s~n", [ClientId, emqttd_message:format(Message)]),
 
     {SenderId, SenderName} = Message#mqtt_message.from,
     %Sender =  Message#mqtt_message.sender,
@@ -247,7 +227,7 @@ on_message_delivered(ClientId, Username, Message, _Env) ->
     Timestamp = Message#mqtt_message.timestamp,
 
     Json = mochijson2:encode([
-        {type, <<"delivered">>},
+        {type, <<"message_delivered">>},
         {client_id, ClientId},
         {sender_id, SenderId},
         {sender_name, SenderName},
@@ -266,7 +246,7 @@ on_message_delivered(ClientId, Username, Message, _Env) ->
 
 %%-----------acknowledgement publish start----------------------------%%
 on_message_acked(ClientId, Username, Message, _Env) ->
-    io:format("client ~s acked: ~s~n", [ClientId, emqttd_message:format(Message)]),   
+    %io:format("client ~s acked: ~s~n", [ClientId, emqttd_message:format(Message)]),   
 
     {SenderId, SenderName} = Message#mqtt_message.from,
     %Sender =  Message#mqtt_message.sender,
@@ -276,7 +256,7 @@ on_message_acked(ClientId, Username, Message, _Env) ->
     Timestamp = Message#mqtt_message.timestamp,
 
     Json = mochijson2:encode([
-        {type, <<"acked">>},
+        {type, <<"message_acked">>},
         {client_id, ClientId},
         {username, Username},
         {sender_id, SenderId},
@@ -312,7 +292,7 @@ ekaf_init(_Env) ->
     {ok, _} = application:ensure_all_started(ranch),
     {ok, _} = application:ensure_all_started(ekaf),
 
-    io:format("Init ekaf with ~p~n", [BootstrapBroker]).
+    io:format("Init ekaf [kafka publisher] with ~p~n", [BootstrapBroker]).
 
 
 %% Called when the plugin application stop
